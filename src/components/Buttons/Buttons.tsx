35ceb820a83fe1eaca56905c5fa3c2a5d5c4c41c
vimu26 <akalankavimukthi2@gmail.com>
2024-05-24 14:54:53 +0530

    button section changes

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     display: 'flex',
-    marginBottom: 8,
+    // marginBottom: 0,
   },
   buttonDisabled: {
     backgroundColor: appStyles.Text.color,
