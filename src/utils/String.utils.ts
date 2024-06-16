b69dd790096044bc10ad2a110760aed5029ab24f
vimu26 <akalankavimukthi2@gmail.com>
2024-06-16 16:44:55 +0530

    utill

--- src/utils/String.utils.ts
+++ src/utils/String.utils.ts
@@ -1,3 +1,12 @@
+import {PROFILE_CLICKS} from '../types/types';
+
 export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const capitalizeAndReplace = (input: string) => {
+  if (typeof input !== 'string') {
+    return;
+  }
+  return input.toUpperCase().replace(/ /g, '_') as PROFILE_CLICKS;
 };
