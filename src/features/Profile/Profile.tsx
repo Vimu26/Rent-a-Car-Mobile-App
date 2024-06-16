12438d1868c6684cbe06bc5e88a80788b7b3319d
vimu26 <akalankavimukthi2@gmail.com>
2024-06-16 16:46:03 +0530

    profile

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {appStyles} from '../../themes/Common-theme';
 import TouchableBar from '../../components/TouchableBar/TouchableBar';
+import {capitalizeAndReplace} from '../../utils/String.utils';
+import {PROFILE_CLICKS} from '../../types/types';
 
-const Profile = () => {
+const Profile = ({navigation}: any) => {
+  // const navigation = useNavigation();
+  const onClick = (name: string) => {
+    const screenName = capitalizeAndReplace(name);
+    console.log(screenName);
+    switch (screenName) {
+      case PROFILE_CLICKS.ACCOUNT_INFORMATION:
+        // navigation.navigate('AccountInformation');
+        break;
+      case PROFILE_CLICKS.TERMS_OF_SERVICE:
+        // navigation.navigate('TermsOfService');
+        break;
+      case PROFILE_CLICKS.PRIVACY_POLICY:
+        // navigation.navigate('PrivacyPolicy');
+        break;
+      case PROFILE_CLICKS.LOGOUT:
+        navigation.navigate('SignIn');
+        break;
+      default:
+    }
+  };
   return (
     <View style={styles.mainContainer}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
@@ -14,6 +36,7 @@ const Profile = () => {
             heading={'Account Information'}
             leftIcon={require('../../assets/common/icons8-user-50.png')}
             rightIcon={require('../../assets/common/icons8-forward-50.png')}
+            onPress={onClick}
           />
         </View>
         <View style={styles.settingsContainer}>
@@ -22,15 +45,18 @@ const Profile = () => {
             heading={'Terms of Service'}
             leftIcon={require('../../assets/common/icons8-terms-and-conditions-48.png')}
             rightIcon={require('../../assets/common/icons8-forward-50.png')}
+            onPress={onClick}
           />
           <TouchableBar
             heading={'Privacy Policy'}
             leftIcon={require('../../assets/common/icons8-privacy-policy-48.png')}
             rightIcon={require('../../assets/common/icons8-forward-50.png')}
+            onPress={onClick}
           />
           <TouchableBar
             heading={'Logout'}
             leftIcon={require('../../assets/common/icons8-logout-48.png')}
+            onPress={onClick}
           />
         </View>
       </ScrollView>
