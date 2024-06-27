892d373dbe874a1da2748312ce25b736017eec17
vimu26 <akalankavimukthi2@gmail.com>
2024-06-27 20:56:55 +0530

    navigatioons

--- src/features/Profile/ProfileStackNavigator.tsx
+++ src/features/Profile/ProfileStackNavigator.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Profile from '../../features/Profile/Profile';
 import SignIn from '../auth/SignIn';
+import {SafeAreaView, View, Image, StyleSheet} from 'react-native';
+import {appStyles} from '../../themes/Common-theme';
 
 // import AccountInformation from '../../features/AccountInformation/AccountInformation'; // Adjust path as necessary
 // import TermsOfService from '../../features/TermsOfService/TermsOfService'; // Adjust path as necessary
@@ -11,9 +13,20 @@ import SignIn from '../auth/SignIn';
 const Stack = createStackNavigator();
 
 const ProfileStackNavigator = () => {
-  function headerImage(): React.ReactNode {
-    throw new Error('Function not implemented.');
-  }
+  const headerImage = () => {
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.outer}>
+          <View style={styles.viewHeader}>
+            <Image
+              source={require('../../assets/logo-no-background.png')}
+              style={styles.imageView}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  };
 
   return (
     <Stack.Navigator initialRouteName="ProfilePage">
@@ -39,5 +52,31 @@ const ProfileStackNavigator = () => {
     </Stack.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: appStyles.cardContainer.color,
+  },
+  viewHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: appStyles.screenHeight.height * 0.11,
+    justifyContent: 'center',
+    backgroundColor: '#272f3e',
+    borderBottomRightRadius: 35,
+    borderBottomLeftRadius: 35,
+    overflow: 'hidden',
+  },
+  imageView: {
+    width: appStyles.screenWidth.width * 0.3,
+    height: appStyles.screenHeight.height * 0.08,
+    objectFit: 'fill',
+    marginRight: 10,
+  },
+  outer: {
+    backgroundColor: '#1f2736',
+  },
+});
 
 export default ProfileStackNavigator;
